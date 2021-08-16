d38567af2ce0a68758721863b2db3be6868e0941
mahziyar110 <mahziyar110@gmail.com>
2021-08-16 15:06:59 +0530

    new commit

--- src/components/shape.js
+++ src/components/shape.js
@@ -1,3 +1,16 @@
+import React from "react";
 // declare class
+class Shape extends React.Component {
+    constructor(props) {
+        super(props);
+        const color = "red";
+    }
+    drawShape() {
 
+    }
+    calculateArea(){
+
+    }
+}
 // export class using module.exports
+module.exports = Shape;
